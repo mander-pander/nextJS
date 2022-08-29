f35e8f2460645d400cd1e907d315b553033678d3
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-29 08:52:47 -0700

    list nav created

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>
+  return <div>Home</div>;
 };
 
 export default Home;
