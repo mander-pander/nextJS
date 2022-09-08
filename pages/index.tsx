5de4ad2475aa16fae1434043de420a1a6980cb7e
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-09-08 13:01:01 -0700

    profile, gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import GradientLayout from "../components/gradientLayout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      roundImage
+      color="red"
+      subtitle="profile"
+      title="mander-pander"
+      description="public playlists"
+      image="https://img.freepik.com/premium-vector/baby-panda-waving-paw-cartoon_42750-612.jpg?w=2000"
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
